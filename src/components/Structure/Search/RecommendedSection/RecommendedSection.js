8009ae3cb562ed756228b801b204919fd29a852c
abdulhanan1998 <hananabdul1998@gmail.com>
2025-05-13 11:20:59 +0500

    Commas added in recommended

--- src/components/Structure/Search/RecommendedSection/RecommendedSection.js
+++ src/components/Structure/Search/RecommendedSection/RecommendedSection.js
@@ -1,5 +1,5 @@
 // RecommendedSection.js
-import React, { useCallback, useEffect, useMemo, useState } from 'react';
+import React, {useCallback, useEffect, useMemo, useState} from 'react';
 import {
   FlatList,
   View,
@@ -10,12 +10,12 @@ import {
 } from 'react-native';
 import styles from './style';
 import Regular from '../../../../typography/RegularText';
-import { HeartSvg } from '../../../../assets/svg';
+import {HeartSvg} from '../../../../assets/svg';
 import Bold from '../../../../typography/BoldText';
-import { useSelector } from 'react-redux';
-import { fetchProducts } from '../../../../api/apiServices';
-import { useTranslation } from 'react-i18next';
-import { mvs } from '../../../../util/metrices';
+import {useSelector} from 'react-redux';
+import {fetchProducts} from '../../../../api/apiServices';
+import {useTranslation} from 'react-i18next';
+import {mvs} from '../../../../util/metrices';
 import ProductCardSkeleton from './ProductCardSkeleton';
 
 const RecommendedSection = ({
@@ -33,7 +33,7 @@ const RecommendedSection = ({
   const [apiProducts, setApiProducts] = useState([]);
   const [productsLoading, setProductsLoading] = useState(true);
 
-  const { token, role } = useSelector(state => state.user);
+  const {token, role} = useSelector(state => state.user);
   const favorites = useSelector(state => state.favorites.favorites);
 
   const likedItemes = useMemo(() => {
@@ -44,7 +44,7 @@ const RecommendedSection = ({
     return map;
   }, [favorites]);
 
-  const { t } = useTranslation();
+  const {t} = useTranslation();
 
   const loadProducts = useCallback(async () => {
     setProductsLoading(true);
@@ -71,17 +71,20 @@ const RecommendedSection = ({
   }, [loadProducts]);
 
   const renderRecommendedItem = useCallback(
-    ({ item }) => (
+    ({item}) => (
       <TouchableOpacity
         style={styles.recommendedItem}
         onPress={() => navigateToProductDetails(item.id)}>
         <Image
-          source={{ uri: `https://backend.souqna.net${item.images?.[0]?.path}` }}
+          source={{uri: `https://backend.souqna.net${item.images?.[0]?.path}`}}
           style={styles.recommendedImage}
         />
         <View style={styles.recommendedTextContainer}>
           <Regular style={styles.recommendedTitle}>{item.name}</Regular>
-          <Regular style={styles.recommendedPrice}>${item.price} - USD</Regular>
+          <Regular style={styles.recommendedPrice}>
+            {' '}
+            $ {Number(item.price).toLocaleString()} - USD
+          </Regular>
         </View>
         {role !== 2 && (
           <TouchableOpacity
@@ -100,7 +103,7 @@ const RecommendedSection = ({
     // Create an array of 6 dummy items for skeleton
     const skeletonItems = Array(6)
       .fill(null)
-      .map((_, index) => ({ id: `skeleton-${index}` }));
+      .map((_, index) => ({id: `skeleton-${index}`}));
 
     return (
       <FlatList
@@ -108,7 +111,7 @@ const RecommendedSection = ({
         numColumns={2}
         keyExtractor={item => item.id}
         renderItem={() => <ProductCardSkeleton />}
-        contentContainerStyle={{ paddingBottom: mvs(10) }}
+        contentContainerStyle={{paddingBottom: mvs(10)}}
       />
     );
   };
@@ -116,7 +119,7 @@ const RecommendedSection = ({
   return (
     <View style={styles.recommendedContainer}>
       {!hideTitle && (
-        <Bold style={[{ marginBottom: 10 }, styles.recommendedText]}>
+        <Bold style={[{marginBottom: 10}, styles.recommendedText]}>
           {t('recommendedForYou')}
         </Bold>
       )}
@@ -144,7 +147,7 @@ const RecommendedSection = ({
           }
           ListEmptyComponent={
             !productsLoading && (
-              <View style={{ paddingVertical: mvs(100) }}>
+              <View style={{paddingVertical: mvs(100)}}>
                 <Image
                   source={require('../../../../assets/img/empty.png')}
                   style={{
@@ -153,7 +156,7 @@ const RecommendedSection = ({
                     height: mvs(200),
                   }}
                 />
-                <Text style={{ textAlign: 'center', marginTop: mvs(20) }}>
+                <Text style={{textAlign: 'center', marginTop: mvs(20)}}>
                   {t('noProductsFound')}
                 </Text>
               </View>
