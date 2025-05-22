256762d9b2e3509b8ba1a33884b631ee920474d9
Muhammad Waleed Qurashi <wqurashi0302@gmail.com>
2025-05-22 12:17:07 +0500

    Map bugs fixed

--- src/screens/App/Map/styles.js
+++ src/screens/App/Map/styles.js
@@ -27,7 +27,7 @@ const styles = StyleSheet.create({
   },
   myLocationButton: {
     position: 'absolute',
-    bottom: mvs(110),
+    // bottom: mvs(110),
     right: mvs(18),
     backgroundColor: colors.lightgreen,
     width: mvs(60),
