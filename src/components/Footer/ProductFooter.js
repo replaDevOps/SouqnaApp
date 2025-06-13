a118b199036f7e3711344417440ea8377b862089
Mubashir <mubashir.mughal@replaofficials.com>
2025-06-13 15:30:03 +0500

    -

--- src/components/Footer/ProductFooter.js
+++ src/components/Footer/ProductFooter.js
@@ -12,7 +12,6 @@ import {colors} from '../../util/color';
 import {CallSVG, ChatSVG2, TrashSVG, UpdateSVG} from '../../assets/svg';
 import {useSelector} from 'react-redux';
 import { useTranslation } from 'react-i18next';
-import { useTranslation } from 'react-i18next';
 
 const ProductFooter = ({
   onBuyPress,
